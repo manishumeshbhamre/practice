e11e45ee35a214c597ef1e80dfe8570c9fdad413
Manish Umesh Bhamre <59453791+manishumeshbhamre@users.noreply.github.com>
2021-10-21 15:35:18 +0530

    Update he.sh

--- he.sh
+++ he.sh
@@ -1,3 +1,3 @@
 docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs docker rm
 
-docker images maven-as-* | grep "days ago" | awk '{print $3}' | xargs docker rmi -f
+docker images maven-as-* | grep "minutes ago" | awk '{print $3}' | xargs docker rmi -f
